d5d98483069b3758aa95065756f1c07351a92182
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-08 17:24:58 +0530

    Update : Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,48 @@
 import "./Sidebar.css";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { setActiveLink } from "../../features/navbar/navslice";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const active = useAppSelector((state) => state.nav.activeLink);
+
+  const handleLinkClick = (e: any) => {
+    e.preventDefault();
+    const link = e.target.text;
+    dispatch(setActiveLink(link));
+
+    switch (link) {
+      case 'Home':
+        navigate('/');
+        break;
+      case 'Questions':
+        navigate('/questions');
+        break;
+      case 'Tags':
+        navigate('/tags');
+        break;
+      case 'Users':
+        navigate('/users');
+        break;
+      default:
+        navigate('/');
+        break;
+    }
+  }
+
   return (
     <aside>
-      <a href="#" className="active">Home</a>
+      <a href="#" className={active == 'Home' ? 'active' : ''} onClick={handleLinkClick}>Home</a>
 
       <p>PUBLIC</p>
 
       <div className="sidebar_links">
-        <a href="#">Questions</a>
-        <a href="#">Tags</a>
-        <a href="#">Users</a>
+        <a href="#" className={active == 'Questions' ? 'active' : ''} onClick={handleLinkClick}>Questions</a>
+        <a href="#" className={active == 'Tags' ? 'active' : ''} onClick={handleLinkClick}>Tags</a>
+        <a href="#" className={active == 'Users' ? 'active' : ''} onClick={handleLinkClick}>Users</a>
       </div>
     </aside>
   );
