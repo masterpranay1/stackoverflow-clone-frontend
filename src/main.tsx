6f97f1ab61fe96fea7ef4b020043ebb30a687481
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-08 08:49:17 +0530

    Feat : Redux-toolkit Config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store.ts";
+import { Provider } from "react-redux";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
