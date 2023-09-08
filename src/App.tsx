23ada221d0bd8efbb3cde60db02864c1ab6b2657
masterpranay1 <rajpranaychottu@gmail.com>
2023-09-08 16:20:24 +0530

    Refactor: App ( Homepage )

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./pages/Homepage/Homepage";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div className="container">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
